d85956be66a300b663e8d4c7d157df158a84ac5e
fjepse22 <fjepse22@student.aau.dk>
2025-04-23 12:04:54 +0200

    sockettest

---
+++ replayplusplus/sockettest.cc
@@ -0,0 +1,8 @@
+
+
+// Formål, at finde ud af om vi kan modtage alle pakker fra et interface og sende det til et andet.
+
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
+
