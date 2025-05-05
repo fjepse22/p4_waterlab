9f387208a822e9c336b5899427aef856a6942e12
SSkandboS <144097806+SSkandboS@users.noreply.github.com>
2025-05-05 11:12:55 +0200

    Update script.py
    Added a guide

--- replayplusplus/prod_v1/script.py
+++ replayplusplus/prod_v1/script.py
@@ -12,3 +12,4 @@ while True:
         break
 
     time.sleep(1)  # Optional delay between replays
+#[how to run this script] in the terminal: use the command sudo`python3 script.py`(remember to run it in the same directory as the script and pcapng file)
