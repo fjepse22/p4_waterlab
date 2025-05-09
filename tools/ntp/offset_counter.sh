1f4625e74fb51b4a8d3a57c10928124ea60f2810
fjepse22 <144028671+fjepse22@users.noreply.github.com>
2025-05-09 10:04:00 +0200

    Update offset_counter.sh
    It now uses the sleep_timer and loop_count variables for something

--- tools/ntp/offset_counter.sh
+++ tools/ntp/offset_counter.sh
@@ -7,13 +7,13 @@ sleep_timer=5 # Set this value to the pool interval
 loop_count=10 # set this counter to how many samples you want
 
 
-for i in {1..10}
+for i in {1..$loop_count}
 do
     offset="$(timedatectl timesync-status | grep "Offset")"
     packet_count="$(timedatectl timesync-status | grep "Packet")"
     time="$(date)"
     echo $offset","$packet_count","$time >> stats.csv
-    sleep 3
+    sleep $sleep_timer
     done
 
 echo "Program is done"
