aba6d3750950a0cad85770a7768d53406f7357d4
fjepse22 <144028671+fjepse22@users.noreply.github.com>
2025-05-19 12:25:56 +0200

    Update offset_counter.sh
    fixed the loop, so it actually works.

--- tools/ntp/offset_counter.sh
+++ tools/ntp/offset_counter.sh
@@ -3,11 +3,11 @@
 #Written by Frederik
 
 #makes comma seperated
-sleep_timer=5 # Set this value to the pool interval
-loop_count=10 # set this counter to how many samples you want
+sleep_timer=64 # Set this value to the pool interval
+loop_count=1010 # set this counter to how many samples you want
 
 
-for i in {1..$loop_count}
+for i in $(seq 1 $loop_count);
 do
     offset="$(timedatectl timesync-status | grep "Offset")"
     packet_count="$(timedatectl timesync-status | grep "Packet")"
@@ -21,6 +21,3 @@ echo "Program is done"
 #echo timedatectl timesync-status | grep "Offset" >> file.csv
 #echo , >> file.csv
 #echo date >> file.csv
-
-
-
